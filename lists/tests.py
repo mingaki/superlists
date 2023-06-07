0b3ecbbd65ec252489bbd10f3e53ba2d5978365a
Guan Ming <gm932015@gmail.com>
2023-06-07 23:10:36 +0800

    feat(lists): add Item model

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from .models import Item
+
 
 class SmokeTest(TestCase):
     def test_home_page_returns_correct_html(self):
@@ -10,3 +12,20 @@ class SmokeTest(TestCase):
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertIn("A new list item", response.content.decode())
         self.assertTemplateUsed(response, "home.html")
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "First ever item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Second item"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        self.assertEqual(saved_items[0].text, "First ever item")
+        self.assertEqual(saved_items[1].text, "Second item")
