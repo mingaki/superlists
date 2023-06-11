e19eb93d99346f0690a58749fbc44d836381d8a4
Guan Ming <gm932015@gmail.com>
2023-06-11 17:17:01 +0800

    feat: adjust model so that items are associated with different lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import Item
+from .models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -15,8 +15,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, "list.html")
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text="Item 1")
-        Item.objects.create(text="Item 2")
+        list_ = List.objects.create()
+        Item.objects.create(text="Item 1", list=list_)
+        Item.objects.create(text="Item 2", list=list_)
 
         response = self.client.get("/lists/the-only-list-in-the-world/")
 
@@ -37,18 +38,28 @@ class NewListTest(TestCase):
         self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "First ever item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Second item"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         self.assertEqual(saved_items[0].text, "First ever item")
+        self.assertEqual(saved_items[0].list, list_)
         self.assertEqual(saved_items[1].text, "Second item")
+        self.assertEqual(saved_items[1].list, list_)
